ee59d35e2257d343e562c7796b888026dc2a3d1a
Jan Erik Hanssen <jhanssen@gmail.com>
2013-03-01 22:51:27 -0800

    Use Config the right way

--- src/Semifreddo.cpp
+++ src/Semifreddo.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char **argv)
 {
     Config::registerOption<String>("socket-name", 's', "Unix domain socket to use (default ~/.gelato)", Path::home() + ".gelato");
-    Config::registerOption<String>("help", 'h', "Display this help");
-    Config::registerOption<String>("version", 'v', "Display version");
+    Config::registerOption("help", 'h', "Display this help");
+    Config::registerOption("version", 'v', "Display version");
     if (!Config::parse(argc, argv)) {
         Config::showHelp(stderr);
         return 1;
